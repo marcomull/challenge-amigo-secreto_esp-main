b3c917cddd67cf0cffef211ae374d08877b51ed5
Marco Arias <marcoarias765@gmail.com>
2025-08-08 13:35:52 -0500

    Se agrego la funcion agregarAmigo() y validacion para campos vacios

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+function agregarAmigo() {
+    let nombreAmigo = document.getElementById('amigo').value.trim();
+
+    if (nombreAmigo === "") {
+        alert("Ingrese un nombre de su amigo");
+        return null;
+    } 
+
+    alert(`Ingreso el nombre del amigo: ${(nombreAmigo)}`)
+    return nombreAmigo;
+}
+
